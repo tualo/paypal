56b154e8bad8f71c7c451847ef004a3913f23283
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-05-09 10:07:55 +0200

    tests

--- src/API.php
+++ src/API.php
@@ -21,7 +21,7 @@ class API {
         $db = App::get('session')->getDB();
         try {
             if (!is_null($db)) {
-                $db->direct('insert into paypal_environments (id,val) values ({id},{val}) on duplicate key update val=values(val)', [
+                $db->direct('insert into paypal_environment (id,val) values ({id},{val}) on duplicate key update val=values(val)', [
                     'id' => $id,
                     'val' => $val
                 ]);
@@ -35,8 +35,9 @@ class API {
         if (is_null(self::$ENV)) {
             $db = App::get('session')->getDB();
             try {
+                self::$ENV = [];
                 if (!is_null($db)) {
-                    $data = $db->direct('select id,val from paypal_environments');
+                    $data = $db->direct('select id,val from paypal_environment');
                     foreach ($data as $d) {
                         self::$ENV[$d['id']] = $d['val'];
                     }
@@ -63,29 +64,35 @@ class API {
         }
         return $data;
     }
-    public static function env($key)
+    public static function env($key, $default = null)
     {
         $env = self::getEnvironment();
         if (isset($env[$key])) {
             return $env[$key];
+        }else if (!is_null($default)) {
+            return $default;
         }
         throw new \Exception('Environment ' . $key . ' not found!');
     }
-    
+    public static function client(bool $token=false):Client{
+        $options = [
+            'base_uri' => self::env('base_url'),
+            'timeout'  => 2.0,
+        ];
+        if ($token){
+            $options['headers'] = [
+                'Authorization' => 'Bearer ' . self::env('access_token')
+            ];
+        }
+        return new Client($options);
+    }
     public static function auth()
     {
-
-
-        $client = new Client(
-            [
-                'base_uri' => self::env('sign_base_url'),
-                'timeout'  => 2.0,
-            ]
-        );
-        $response = $client->post('/api/v2/auth', [
-            'json' => [
-                'api_key' => self::env('api_key'),
-                'api_secret' => self::env('api_secret')
+        
+        $response = self::client()->post('/v1/oauth2/token', [
+            'auth' => [self::env('CLIENT_ID'), self::env('CLIENT_SECRET')],
+            'form_params' => [
+                'grant_type' => 'client_credentials'
             ]
         ]);
         $code = $response->getStatusCode(); // 200
@@ -97,11 +104,178 @@ class API {
         $result = json_decode($response->getBody()->getContents(), true);
         if (isset($result['access_token'])) {
             self::addEnvrionment('access_token', $result['access_token']);
-            self::addEnvrionment('access_token_expires_at', $result['access_token_expires_at']);
-
-            self::addEnvrionment('refresh_token', $result['refresh_token']);
-            self::addEnvrionment('refresh_token_expires_at', $result['refresh_token_expires_at']);
+            self::addEnvrionment('app_id', $result['app_id']);
+            self::addEnvrionment('access_token_expires_at', time()+intval($result['expires_in']));
+            self::addEnvrionment('nonce', $result['nonce']);
+            self::addEnvrionment('scope', $result['scope']);
         }
+        return $result;
+    }
+
+    public static function userProfile(){
+        $response = self::client(true)->get('/v1/identity/openidconnect/userinfo?schema=openid');
+        $code = $response->getStatusCode(); // 200
+        $reason = $response->getReasonPhrase(); // OK
+
+        if ($code != 200) {
+            throw new \Exception($reason);
+        }
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+
+    public static function createOrder(
+        float $value,
+        string $curreny,
+        string $invoicenumber,
+        string $custom_id,
+
+    ){
+
+        $options = [
+            'base_uri' => self::env('base_url'),
+            'timeout'  => 2.0,
+        ];
+        $options['headers'] = [
+            'Authorization' => 'Bearer ' . self::env('access_token'),
+            'PayPal-Request-Id' => time()
+        ];
+
+        $o = '{
+            "intent": "CAPTURE",
+            "purchase_units": [
+              {
+                "amount": {
+                  "currency_code": "USD",
+                  "value": "100.00"
+                },
+                "shipping": {
+                    "type": "SHIPPING",
+                    "address": {
+                      "address_line_1": "2211 N First Street",
+                      "address_line_2": "Building 17",
+                      "admin_area_2": "San Jose",
+                      "admin_area_1": "CA",
+                      "postal_code": "95131",
+                      "country_code": "US"
+                    }
+                }
+              }
+            ],
+
+            
+            "payment_source": {
+              "paypal": {
+                "experience_context": {
+                  "payment_method_preference": "IMMEDIATE_PAYMENT_REQUIRED",
+                  "brand_name": "EXAMPLE INC",
+                  "locale": "en-US",
+                  "landing_page": "LOGIN",
+                  "shipping_preference": "SET_PROVIDED_ADDRESS",
+                  "user_action": "PAY_NOW",
+                  "return_url": "https://world-contact.systems/returnUrl",
+                  "cancel_url": "https://world-contact.systems/cancelUrl"
+                }
+              }
+            }
+          }' ;
+
+        $json = json_decode($o,true);
+        $json['purchase_units'][0]['amount']['value'] = number_format($value,2,'.','');
+        $json['purchase_units'][0]['amount']['currency_code'] = $curreny;
+        /*
+        $json['purchase_units'][0]['invoice_id'] = $invoicenumber;
+        $json['purchase_units'][0]['custom_id'] = $custom_id;
+        */
+
+        $json['purchase_units'][0]['shipping']['address']['address_line_1'] = 'Thomas Hoffmann';
+        $json['purchase_units'][0]['shipping']['address']['address_line_2'] = 'Hauptstr. 1';
+        $json['purchase_units'][0]['shipping']['address']['admin_area_2'] = 'Hamburg';
+        $json['purchase_units'][0]['shipping']['address']['admin_area_1'] = 'Hamburg';
+        $json['purchase_units'][0]['shipping']['address']['postal_code'] = '20095';
+        $json['purchase_units'][0]['shipping']['address']['country_code'] = 'DE';
+
+
+
+        $json['payment_source']['paypal']['experience_context']['brand_name'] = self::env('brand_name','tualo solutions GmbH');
+        //$json['payment_source']['paypal']['experience_context']['locale'] = self::env('locale','de_DE');
+        $json['payment_source']['paypal']['experience_context']['landing_page'] = self::env('landing_page','LOGIN');
+        $json['payment_source']['paypal']['experience_context']['shipping_preference'] = self::env('shipping_preference','SET_PROVIDED_ADDRESS');
+        $json['payment_source']['paypal']['experience_context']['user_action'] = self::env('user_action','PAY_NOW');
+        $json['payment_source']['paypal']['experience_context']['return_url'] = self::env('return_url','https://world-contact.systems/returnUrl');
+        $json['payment_source']['paypal']['experience_context']['cancel_url'] = self::env('cancel_url','https://world-contact.systems/cancelUrl');
+
+        $response = (new Client($options))->post('/v2/checkout/orders',[
+            'json' => $json
+        ]
+        );
+
+        $code = $response->getStatusCode(); // 200
+        $reason = $response->getReasonPhrase(); // OK
+
+        if ($code != 200) {
+            throw new \Exception($reason);
+        }
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+    
+
+    public static function orderDetails(
+        string $authorization_id
+    ):array
+    {
+        $response = self::client(true)->get('/v2/checkout/orders/'.$authorization_id.'');
+        $code = $response->getStatusCode(); // 200
+        $reason = $response->getReasonPhrase(); // OK
+
+        if ($code != 200) {
+            throw new \Exception($reason);
+        }
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public static function authorizeOrder(
+        string $authorization_id
+    ):array
+    {
+        $details = self::orderDetails($authorization_id);
+
+        $response = self::client(true)->post('/v2/checkout/orders/'.$authorization_id.'/authorize',[
+            'json' => [
+                'payment_source' => $details['payment_source']
+            ]
+        ]);
+        $code = $response->getStatusCode(); // 200
+        $reason = $response->getReasonPhrase(); // OK
+
+        if ($code != 200) {
+            throw new \Exception($reason);
+        }
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public static function captureOrder(
+        string $authorization_id
+    ):array
+    {
+        $details = self::orderDetails($authorization_id);
+
+        $response = self::client(true)->post('/v2/checkout/orders/'.$authorization_id.'/capture',[
+            'json' => [
+                'payment_source' => $details['payment_source']
+            ]
+        ]);
+        $code = $response->getStatusCode(); // 200
+        $reason = $response->getReasonPhrase(); // OK
+
+        if ($code != 200) {
+            throw new \Exception($reason);
+        }
+        $result = json_decode($response->getBody()->getContents(), true);
         return $result;
     }
 
--- src/Routes/Test.php
+++ src/Routes/Test.php
@@ -3,32 +3,98 @@ namespace Tualo\Office\Paypal\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
-use Tualo\Office\Stripe\API;
+use Tualo\Office\Paypal\API;
 
 class Test implements IRoute{
     
     public static function register(){
-        BasicRoute::add('/paypal/test',function($matches){
+        BasicRoute::add('/paypal/auth',function($matches){
             try {
-                $db = App::get('session')->getDB();
-
-                $data = API::addShorthandCheckout(
-                    'https://tualo.de',
-                    'https://tualo.de',
-                    'Testprodukt',
-                    'Testprodukt Beschreibung',
-                    1.99,
-                    1
-                );
+                API::init();
+                $data = API::auth();
                 App::result('success',true);
                 App::result('data',$data);
                 App::contenttype('application/json');
-
             }catch(\Exception $e){
                 echo $e->getMessage();
                 http_response_code(400);
             }
         },['get'],true);
 
+        BasicRoute::add('/paypal/profile',function($matches){
+            try {
+                API::init();
+                $data = API::userProfile();
+                App::result('success',true);
+                App::result('data',$data);
+                App::contenttype('application/json');
+            }catch(\Exception $e){
+                echo $e->getMessage();
+                http_response_code(400);
+            }
+        },['get'],true);
+
+        BasicRoute::add('/paypal/createorder',function($matches){
+            try {
+                API::init();
+                $data = API::createOrder(
+                    10,
+                    'EUR',
+                    'RN1623712',
+                    '13718'
+                );
+                App::result('success',true);
+                App::result('data',$data);
+                App::contenttype('application/json');
+            }catch(\Exception $e){
+                echo $e->getMessage();
+                http_response_code(400);
+            }
+        },['get'],true);
+
+        BasicRoute::add('/paypal/details',function($matches){
+            try {
+                API::init();
+                $data = API::orderDetails(
+                    $_REQUEST['id'],
+                );
+                App::result('success',true);
+                App::result('data',$data);
+                App::contenttype('application/json');
+            }catch(\Exception $e){
+                echo $e->getMessage();
+                http_response_code(400);
+            }
+        },['get'],true);
+
+        BasicRoute::add('/paypal/authorize',function($matches){
+            try {
+                API::init();
+                $data = API::authorizeOrder(
+                    $_REQUEST['id'],
+                );
+                App::result('success',true);
+                App::result('data',$data);
+                App::contenttype('application/json');
+            }catch(\Exception $e){
+                echo $e->getMessage();
+                http_response_code(400);
+            }
+        },['get'],true);
+
+        BasicRoute::add('/paypal/capture',function($matches){
+            try {
+                API::init();
+                $data = API::captureOrder(
+                    $_REQUEST['id'],
+                );
+                App::result('success',true);
+                App::result('data',$data);
+                App::contenttype('application/json');
+            }catch(\Exception $e){
+                echo $e->getMessage();
+                http_response_code(400);
+            }
+        },['get'],true);
     }
 }
