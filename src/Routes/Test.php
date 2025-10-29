011c38373979d2e883cd8ca31c3048782ba603cc
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:33:59 +0100

    Version bump to 1.0.2

--- src/Routes/Test.php
+++ src/Routes/Test.php
@@ -1,40 +1,44 @@
 <?php
+
 namespace Tualo\Office\Paypal\Routes;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 use Tualo\Office\Paypal\API;
 
-class Test implements IRoute{
-    
-    public static function register(){
-        BasicRoute::add('/paypal/auth',function($matches){
+class Test extends \Tualo\Office\Basic\RouteWrapper
+{
+
+    public static function register()
+    {
+        BasicRoute::add('/paypal/auth', function ($matches) {
             try {
                 API::init();
                 $data = API::auth();
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        BasicRoute::add('/paypal/profile',function($matches){
+        BasicRoute::add('/paypal/profile', function ($matches) {
             try {
                 API::init();
                 $data = API::userProfile();
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        BasicRoute::add('/paypal/createorder',function($matches){
+        BasicRoute::add('/paypal/createorder', function ($matches) {
             try {
                 API::init();
                 $data = API::createOrder(
@@ -43,58 +47,58 @@ class Test implements IRoute{
                     'RN1623712',
                     '13718'
                 );
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        BasicRoute::add('/paypal/details',function($matches){
+        BasicRoute::add('/paypal/details', function ($matches) {
             try {
                 API::init();
                 $data = API::orderDetails(
                     $_REQUEST['id'],
                 );
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        BasicRoute::add('/paypal/authorize',function($matches){
+        BasicRoute::add('/paypal/authorize', function ($matches) {
             try {
                 API::init();
                 $data = API::authorizeOrder(
                     $_REQUEST['id'],
                 );
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        BasicRoute::add('/paypal/capture',function($matches){
+        BasicRoute::add('/paypal/capture', function ($matches) {
             try {
                 API::init();
                 $data = API::captureOrder(
                     $_REQUEST['id'],
                 );
-                App::result('success',true);
-                App::result('data',$data);
+                App::result('success', true);
+                App::result('data', $data);
                 App::contenttype('application/json');
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get'],true);
+        }, ['get'], true);
     }
 }
--- src/Routes/Webhook.php
+++ src/Routes/Webhook.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Tualo\Office\Paypal\Routes;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
@@ -7,32 +9,33 @@ use Stripe\StripeClient;
 use Stripe\Webhook as StripeWebhook;
 use Stripe\Exception\SignatureVerificationException;
 
-class Webhook implements IRoute{
-    
-    public static function register(){
-        BasicRoute::add('/paypal/webhook',function($matches){
+class Webhook extends \Tualo\Office\Basic\RouteWrapper
+{
+
+    public static function register()
+    {
+        BasicRoute::add('/paypal/webhook', function ($matches) {
             try {
                 $db = App::get('session')->getDB();
 
 
                 $payload = @file_get_contents('php://input');
-                $db->direct('insert into paypal_webhook ( eventtype,eventdata) values ( {eventtype},{eventdata})',
+                $db->direct(
+                    'insert into paypal_webhook ( eventtype,eventdata) values ( {eventtype},{eventdata})',
                     [
-                        'eventtype'=>'incoming',
-                        'eventdata'=>$payload
+                        'eventtype' => 'incoming',
+                        'eventdata' => $payload
                     ]
                 );
-                
 
-                
+
+
                 http_response_code(200);
                 exit();
-
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(400);
             }
-        },['get','post','put','delete'],true);
-
+        }, ['get', 'post', 'put', 'delete'], true);
     }
 }
